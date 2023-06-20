55f9e255b4fa92a473ee68b2a34e372632f60de1
Araki <c0b220062b@edu.teu.ac.jp>
2023-06-20 16:02:33 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,13 @@
 import random
 import sys
 import pygame as pg
+delta = {                  
+        pg.K_UP:(0,-5), # 移動量の上
+        pg.K_DOWN:(0,+5), # 移動量の下
+        pg.K_LEFT:(-5,0), # 移動量の左
+        pg.K_RIGHT:(+5,0) #　移動量の右
+        }
+
 
 
 WIDTH, HEIGHT = 1600, 900
@@ -12,6 +19,9 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+     # こうかとんSurface（kk_img）からこうかとんRect（kk_rct）を抽出する
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
     bd_img = pg.Surface((20,20)) # 爆弾の作成
     pg.draw.circle(bd_img,(255,0,0),(10,10),10) # 爆弾を設定
     bd_img.set_colorkey((0,0,0)) # 背景を透過
@@ -28,14 +38,26 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]  # 合計移動量
+        for k, mv in delta.items():
+            if key_lst[k]: 
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv)
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img,kk_rct)
         screen.blit(bd_img,bd_rct)
-        bd_rct.move_ip(vx,vy)
+        bd_rct.move_ip(vx,vy)# 練習２
 
         pg.display.update()
         tmr += 1
+        screen.blit(bg_img, [0, 0])
+        screen.blit(bb_img, bb_rct)
+        bb_rct.move_ip(vx, vy)
         clock.tick(10)
 
 
